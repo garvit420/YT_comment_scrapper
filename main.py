8d91dd02c0deeab78385cfbf0fdf6df496392af4
garv420 <garvit.23bcs10091@ms.sst.scaler.com>
2024-05-31 19:18:14 +0530

    added concurrency

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# from itertools import islice
-# from youtube_comment_downloader import *
-# downloader = YoutubeCommentDownloader()
-# comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=YxWlaYCA8MU', sort_by=SORT_BY_POPULAR)
-# for comment in islice(comments, 10):
-#     print(comment)
-
 import json
 from itertools import islice
 from youtube_comment_downloader import YoutubeCommentDownloader, SORT_BY_POPULAR
+import concurrent.futures
 
 # Initialize the downloader
 downloader = YoutubeCommentDownloader()
 
 # Get comments from the YouTube video
-comments = downloader.get_comments_from_url('https://www.youtube.com/watch?v=kffacxfA7G4', sort_by=SORT_BY_POPULAR)
+video_url = 'https://www.youtube.com/watch?v=kffacxfA7G4'
 
-# Collect the first 10 comments
-comments_list = []
-for comment in islice(comments, 400):
-    comments_list.append(comment)
+# Define the number of comments to fetch
+num_comments = 10000
+
+# Fetch comments concurrently
+def fetch_comments():
+    return list(islice(downloader.get_comments_from_url(video_url, sort_by=SORT_BY_POPULAR), num_comments))
+
+# Use ThreadPoolExecutor for concurrent processing
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    comments_list = executor.submit(fetch_comments).result()
 
 # Save the comments to a JSON file
 with open('comments.json', 'w') as json_file:
